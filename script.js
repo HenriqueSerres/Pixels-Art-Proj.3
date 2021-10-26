e5ed6d5ab396e846e0abf977d9af7dd3b01abb12
Henrique Serres <henrique@gmail.com>
2021-10-26 00:01:17 -0300

    exercicio 6 e 7

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const pixelsGrade = document.querySelector('#pixel-board');
 
-
 function creatPixels(){
   for (let index = 0; index < 5; index += 1) {
     let pixel = document.createElement('div');
@@ -15,3 +14,26 @@ creatPixels();
 for (let index = 0; index < 4; index += 1) {  
   let gradePixels = creatPixels();
 };
+// exercício 6
+const whitePixel = document.querySelector(".pixel");
+const blackPixel = document.querySelector("#black");
+const greenPixel = document.querySelector("#green");
+const yellowPixel = document.querySelector("#yellow");
+const redPixel = document.querySelector("#red");
+
+function changePixelColor() {
+ const startBlack = document.querySelectorAll("#pixel-board");
+ startBlack.addEventListener('click', function(event) {
+  if (event.target === whitePixel) {
+    event.target = blackPixel;
+  } else {
+    event.target = whitePixel;
+  }
+});
+}
+changePixelColor();
+
+// exercício 7
+//function changePixelColor(event) {
+//   let colorPixelChoice = document.querySelector
+// }
